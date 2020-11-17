6ef93460bbc81fc7b3664342786d1c7596caa42f
3iswajit <66627619+3iswajit@users.noreply.github.com>
2020-11-17 18:53:50 +0530

    4th welcome

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,11 +3,17 @@ import './App.css';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import NotFound from './component/notFound';
 import Home from './component/Home';
+import DashBoard from './component/DashBoard';
+import Register from './component/Register';
+import Login from './component/Login';
 class App extends Component {
 	render() {
 		return (
 			<Switch>
-				<Route path='/home' component={Home} />
+				<Route path='/register' component={Register} />
+				<Route path='/DashBoard' component={DashBoard} />
+				<Redirect from='/login' to='/DashBoard' />
+				<Route path='/' component={Home} />
 				<Route path='/not-found' component={NotFound} />
 				<Redirect to='/not-found' />
 			</Switch>
---
+++ react-app/src/component/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Redirect } from 'react-router-dom';
+const Login = () => {
+	return (
+		<React.Fragment>
+			<h1>Login</h1>
+		</React.Fragment>
+	);
+};
+
+export default Login;
